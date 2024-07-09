a35b8b264954bfa21fcc057c89167183950d2df9
Gigo Dev <guyrick21@gmail.com>
2024-07-09 21:03:04 +0300

    mistertoy backend server

---
+++ services/toy.service.js
@@ -0,0 +1,74 @@
+
+import fs from 'fs'
+import { utilService } from './util.service.js'
+import { loggerService } from './logger.service.js'
+
+export const toyService = {
+    query,
+    getById,
+    remove,
+    save
+}
+
+const toys = utilService.readJsonFile('data/toy.json')
+
+function query(filterBy = { txt: '' }) {
+
+    // Filter:
+
+     let toysToReturn = toys
+
+    if (filterBy.txt) {
+        const regExp = new RegExp(filterBy.txt, 'i')
+        toysToReturn = toysToReturn.filter(toy => regExp.test(toy.name))
+    }
+
+    if (filterBy !== 'all') {
+
+        toysToReturn = toysToReturn.filter(toy => filterBy.stock === 'inStock' ? toy.inStock : !toy.inStock)
+    }
+
+    return Promise.resolve(toysToReturn)
+}
+
+function getById(toyId) {
+    const toy = toys.find(toy => toy._id === toyId)
+    return Promise.resolve(toy)
+}
+
+function remove(toyId) {
+    const idx = toys.findIndex(toy => toy._id === toyId)
+    if (idx === -1) return Promise.reject('No Such Toy')
+    const toy = toys[idx]
+    toys.splice(idx, 1)
+    return _saveToysToFile()
+}
+
+function save(toy) {
+    if (toy._id) {
+        const toyToUpdate = toys.find(currToy => currToy._id === toy._id)
+        toyToUpdate.vendor = toy.vendor
+        toyToUpdate.speed = toy.speed
+        toyToUpdate.price = toy.price
+        toy = toyToUpdate
+    } else {
+        toy._id = utilService.makeId()
+        toys.push(toy)
+    }
+
+    return _saveToysToFile().then(() => toy)
+}
+
+
+function _saveToysToFile() {
+    return new Promise((resolve, reject) => {
+        const data = JSON.stringify(toys, null, 4)
+        fs.writeFile('data/toy.json', data, (err) => {
+            if (err) {
+                loggerService.error('Cannot write to toys file', err)
+                return reject(err)
+            }
+            resolve()
+        })
+    })
+}
